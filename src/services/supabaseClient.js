c276fbd338158c448586597182d867e3a6de9a3d
Elly Onunga <onungaelly@gmail.com>
2025-07-07 17:57:27 +0300

    updated supabase key

--- src/services/supabaseClient.js
+++ src/services/supabaseClient.js
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://qyewhmttvkziolvrhqmy.supabase.co'
-const supabaseKey = process.env.SUPABASE_KEY
+const supabaseUrl = 'https://qyewhmttvkziolvrhqmy.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF5ZXdobXR0dmt6aW9sdnJocW15Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE4OTE2NzcsImV4cCI6MjA2NzQ2NzY3N30.0E2tWq148HvT0xgtDKY-Eferw2Ymr5pZPSqrcigzexM';
 
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
